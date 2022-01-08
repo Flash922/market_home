d59bbf85476d59344a905364f74fe8451e1dfe29
Flash922 <Flash92@gmail.com>
2022-01-08 16:58:33 +0300

    Added Section slider

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+  $('.home-slider__wrapper').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: false,
+    arrows: true,
+  });
+
+});
+
