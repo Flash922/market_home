901222d288af8a42fbe594314381770cd57f38a2
Flash922 <Flash92@gmail.com>
2022-01-11 18:11:59 +0300

    Added section Join and slider-two

--- app/js/main.js
+++ app/js/main.js
@@ -8,12 +8,23 @@ $(function(){
     adaptiveHeight: false,
     arrows: true,
   });
+  $('.home-slider__wrapper-two').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    arrows: true,
+  });
   $('.rate-star').rateYo({
     rating: 5,
     starWidth: '14px',
     ratedFill: '#ffc000',
     readOnly: true,
   });
-
+  var mixer = mixitup('.n-product__container'
+  // , {controls: {
+  //     toggleDefault: 'none',
+  //   },}
+  );
 });
 
